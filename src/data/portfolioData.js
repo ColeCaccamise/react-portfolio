3a13511230346a297f7c2d724bc00b1cca5424fb
Cole Caccamise <colecaccamise@gmail.com>
2022-01-08 02:37:51 -0500

    updated portfolion site preview img

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -2,6 +2,7 @@ import { FaGithub, FaInstagram, FaDribbble } from 'react-icons/fa';
 import portfolioSite from '../assets/portfolio-site.png';
 import guessingGame from '../assets/guessing-game.png';
 import readingList from '../assets/reading-list.png';
+import * as uuid from 'uuid';
 
 const portfolioData = [{
   'img': {
@@ -26,7 +27,7 @@ const portfolioData = [{
     'children': <FaGithub />,
     'goTo': 'https://github.com/ColeCaccamise/react-portfolio',
     'external': true}
-  ]
+  ],
 },
 {
   'img': {
